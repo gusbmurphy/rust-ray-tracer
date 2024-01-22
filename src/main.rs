81c1cebd1e0f93db344ab6dbaa93efb9f375bf0c
Augustus Murphy <gusbmurphy@gmail.com>
2024-01-21 21:58:54 -0600

    Write PPM file from little projectile example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use crate::color::Color;
 pub use crate::projectile_tick::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 mod canvas;
 mod color;
@@ -13,20 +16,31 @@ mod prelude {
     pub use crate::tuple::*;
 }
 
+use canvas::Canvas;
+use ppm::create_ppm_from_canvas;
 use prelude::*;
 use vector::Vector;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut projectile = Projectile::new(Point::new(0.0, 0.1, 0.0), Vector::new(1.0, 1.0, 1.0));
 
     let environment = Environment::new(Vector::new(0.0, -0.1, 0.0), Vector::new(0.0, -0.1, 0.0));
+
+    let mut canvas = Canvas::new(15, 15);
 
     while projectile.get_position().get_y() > 0.0 {
         let x = projectile.get_position().get_x();
         let y = projectile.get_position().get_y();
         let z = projectile.get_position().get_z();
 
+        canvas.write_pixel(x.round() as usize, y.round() as usize, Color::new(1.0, 0.0, 0.0));
         println!("Projectile is at {:?}", (x, y, z));
         projectile = tick(&environment, projectile);
     }
+
+    let ppm_data = create_ppm_from_canvas(canvas);
+    let mut file = File::create("projectile.ppm")?;
+    file.write_all(ppm_data.as_bytes())?;
+
+    Ok(())
 }
