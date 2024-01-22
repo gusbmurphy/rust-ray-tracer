fc59c3d9cd251bfebb152065b992b8f126f38a9d
Augustus Murphy <gusbmurphy@gmail.com>
2024-01-21 21:03:11 -0600

    Implement splitting lines beyond PPM line length limit

--- src/canvas.rs
+++ src/canvas.rs
@@ -58,6 +58,7 @@ fn create_default_row(length: u8) -> Vec<Color> {
 }
 
 const MAX_PPM_COLOR_VALUE: u8 = 255;
+const MAX_PPM_LINE_LENGTH: u8 = 70;
 
 pub fn create_ppm_from_canvas(canvas: Canvas) -> String {
     let mut header = "P3\n".to_owned();
@@ -67,25 +68,42 @@ pub fn create_ppm_from_canvas(canvas: Canvas) -> String {
     let mut pixel_data = String::new();
 
     for row in canvas.get_rows() {
-        for (i, color) in row.iter().enumerate() {
-            if i > 0 && i < row.len() {
-                pixel_data.push(' ');
-            }
+        let mut row_string = String::new();
 
-            pixel_data.push_str(convert_color_to_ppm_pixel(color).as_str());
+        for color in row {
+            let ppm_values = convert_color_to_ppm_values(color);
+
+            for ppm_value in ppm_values {
+                let value_string = ppm_value.to_string();
+
+                if row_string.chars().count() + 1 + value_string.chars().count()
+                    < MAX_PPM_LINE_LENGTH as usize
+                {
+                    if !row_string.is_empty() {
+                        row_string.push(' ');
+                    }
+                    row_string.push_str(&value_string);
+                } else {
+                    row_string.push('\n');
+                    pixel_data.push_str(&row_string);
+                    row_string = String::from(value_string);
+                }
+            }
         }
-        pixel_data.push('\n')
+
+        pixel_data.push_str(&row_string);
+        pixel_data.push('\n');
     }
 
     return String::from(header + pixel_data.as_str());
 }
 
-fn convert_color_to_ppm_pixel(color: &Color) -> String {
+fn convert_color_to_ppm_values(color: &Color) -> [u8; 3] {
     let r = convert_color_value_to_ppm_value(color.get_r());
     let b = convert_color_value_to_ppm_value(color.get_b());
     let g = convert_color_value_to_ppm_value(color.get_g());
 
-    return String::from(format!("{} {} {}", r, b, g));
+    return [r, b, g];
 }
 
 fn convert_color_value_to_ppm_value(value: f64) -> u8 {
@@ -173,6 +191,38 @@ mod test {
             255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n\
             0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n\
             0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n\
+        ";
+
+        assert_eq!(pixel_data, expected_pixel_data);
+    }
+
+    #[test]
+    fn splitting_long_ppm_lines() {
+        let mut canvas = Canvas::new(10, 2);
+
+        for y in 0..2 {
+            for x in 0..10 {
+                canvas.write_pixel(x, y, Color::new(1.0, 0.8, 0.6));
+            }
+        }
+
+        let ppm = create_ppm_from_canvas(canvas);
+
+        let mut pixel_data = String::new();
+
+        for (i, line) in ppm.lines().enumerate() {
+            if i > 2 && i < 7 {
+                pixel_data.push_str(line);
+                pixel_data.push('\n');
+            }
+        }
+
+        // No line can be more than 70 characters long...
+        let expected_pixel_data = "\
+            255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n\
+            153 255 204 153 255 204 153 255 204 153 255 204 153\n\
+            255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n\
+            153 255 204 153 255 204 153 255 204 153 255 204 153\n\
         ";
 
         assert_eq!(pixel_data, expected_pixel_data);
