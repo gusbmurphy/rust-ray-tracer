67b74bb5a3a6e6fd51cc4bae036e95e4e41c19e4
Augustus Murphy <gusbmurphy@gmail.com>
2024-02-11 20:46:06 -0600

    Use array instead of vector for Matrix values

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,11 +2,11 @@ use std::ops::Mul;
 
 #[derive(PartialEq)]
 struct Matrix {
-    values: Vec<Vec<f32>>,
+    values: [[f32; 4]; 4],
 }
 
 impl Matrix {
-    pub fn new(values: Vec<Vec<f32>>) -> Self {
+    pub fn new(values: [[f32; 4]; 4]) -> Self {
         return Matrix { values };
     }
 
@@ -21,7 +21,7 @@ impl Mul<Matrix> for Matrix {
 
     fn mul(self, rhs: Matrix) -> Self::Output {
         let size = self.values.len();
-        let mut result_values = create_empty_values(size);
+        let mut result_values = [[0.0; 4]; 4];
 
         for column_index in 0..size {
             for row_index in 0..size {
@@ -57,33 +57,17 @@ impl Mul<[f32; 4]> for Matrix {
     }
 }
 
-fn create_empty_values(size: usize) -> Vec<Vec<f32>> {
-    let mut values: Vec<Vec<f32>> = Vec::new();
-
-    for _column in 0..size {
-        let mut row: Vec<f32> = Vec::new();
-
-        for _row in 0..size {
-            row.push(0.0);
-        }
-
-        values.push(row);
-    }
-
-    return values;
-}
-
 #[cfg(test)]
 mod test {
     use super::Matrix;
 
     #[test]
     fn building_4_by_4_matrix() {
-        let matrix = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![5.5, 6.5, 7.5, 8.5],
-            vec![9.0, 10.0, 11.0, 12.0],
-            vec![13.5, 14.5, 15.5, 16.5],
+        let matrix = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
         ]);
 
         assert_eq!(matrix.get_value((0, 0)).unwrap().to_owned(), 1.0);
@@ -97,26 +81,31 @@ mod test {
 
     #[test]
     fn get_value_returns_nothing_outside_of_bounds() {
-        let matrix = Matrix::new(vec![vec![1.0]]);
+        let matrix = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
+        ]);
 
-        assert_eq!(matrix.get_value((1, 0)), None);
-        assert_eq!(matrix.get_value((0, 1)), None);
+        assert_eq!(matrix.get_value((4, 0)), None);
+        assert_eq!(matrix.get_value((0, 4)), None);
     }
 
     #[test]
     fn matrix_equality() {
-        let matrix1 = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![5.5, 6.5, 7.5, 8.5],
-            vec![9.0, 10.0, 11.0, 12.0],
-            vec![13.5, 14.5, 15.5, 16.5],
+        let matrix1 = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
         ]);
 
-        let matrix2 = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![5.5, 6.5, 7.5, 8.5],
-            vec![9.0, 10.0, 11.0, 12.0],
-            vec![13.5, 14.5, 15.5, 16.5],
+        let matrix2 = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
         ]);
 
         assert!(matrix1 == matrix2)
@@ -124,18 +113,18 @@ mod test {
 
     #[test]
     fn matrix_non_equality() {
-        let matrix1 = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![5.5, 6.5, 7.5, 8.5],
-            vec![9.0, 10.0, 11.0, 12.0],
-            vec![13.5, 14.5, 15.5, 16.5],
+        let matrix1 = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
         ]);
 
-        let matrix2 = Matrix::new(vec![
-            vec![9999.0, 2.0, 3.0, 4.0],
-            vec![5.5, 6.5, 7.5, 8.5],
-            vec![9.0, 10.0, 11.0, 12.0],
-            vec![13.5, 14.5, 15.5, 16.5],
+        let matrix2 = Matrix::new([
+            [9999.0, 2.0, 3.0, 4.0],
+            [5.5, 6.5, 7.5, 8.5],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.5, 14.5, 15.5, 16.5],
         ]);
 
         assert!(matrix1 != matrix2)
@@ -143,26 +132,26 @@ mod test {
 
     #[test]
     fn matrix_multiplication() {
-        let matrix1 = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![5.0, 6.0, 7.0, 8.0],
-            vec![9.0, 8.0, 7.0, 6.0],
-            vec![5.0, 4.0, 3.0, 2.0],
+        let matrix1 = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 8.0, 7.0, 6.0],
+            [5.0, 4.0, 3.0, 2.0],
         ]);
 
-        let matrix2 = Matrix::new(vec![
-            vec![-2.0, 1.0, 2.0, 3.0],
-            vec![3.0, 2.0, 1.0, -1.0],
-            vec![4.0, 3.0, 6.0, 5.0],
-            vec![1.0, 2.0, 7.0, 8.0],
+        let matrix2 = Matrix::new([
+            [-2.0, 1.0, 2.0, 3.0],
+            [3.0, 2.0, 1.0, -1.0],
+            [4.0, 3.0, 6.0, 5.0],
+            [1.0, 2.0, 7.0, 8.0],
         ]);
 
         let result = matrix1 * matrix2;
-        let expected = Matrix::new(vec![
-            vec![20.0, 22.0, 50.0, 48.0],
-            vec![44.0, 54.0, 114.0, 108.0],
-            vec![40.0, 58.0, 110.0, 102.0],
-            vec![16.0, 26.0, 46.0, 42.0],
+        let expected = Matrix::new([
+            [20.0, 22.0, 50.0, 48.0],
+            [44.0, 54.0, 114.0, 108.0],
+            [40.0, 58.0, 110.0, 102.0],
+            [16.0, 26.0, 46.0, 42.0],
         ]);
 
         for column in 0..4 {
@@ -177,11 +166,11 @@ mod test {
 
     #[test]
     fn matrix_multiplication_by_tuple() {
-        let matrix = Matrix::new(vec![
-            vec![1.0, 2.0, 3.0, 4.0],
-            vec![2.0, 4.0, 4.0, 2.0],
-            vec![8.0, 6.0, 4.0, 1.0],
-            vec![0.0, 0.0, 0.0, 1.0],
+        let matrix = Matrix::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [2.0, 4.0, 4.0, 2.0],
+            [8.0, 6.0, 4.0, 1.0],
+            [0.0, 0.0, 0.0, 1.0],
         ]);
 
         let tuple = [1.0, 2.0, 3.0, 1.0];
