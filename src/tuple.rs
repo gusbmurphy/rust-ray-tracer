1c4e9a3e24495aede07754d11b40a32b039e6e5f
Augustus Murphy <gusbmurphy@gmail.com>
2024-02-25 17:19:21 -0600

    Apply cargo formatting in tuple.rs

--- src/tuple.rs
+++ src/tuple.rs
@@ -5,10 +5,8 @@ pub trait Tuple {
     fn get_w(&self) -> f64;
 
     fn get_magnitude(&self) -> f64 {
-        ((self.get_x().powi(2)
-            + self.get_y().powi(2)
-            + self.get_z().powi(2)
-            + self.get_w().powi(2)) as f64)
+        ((self.get_x().powi(2) + self.get_y().powi(2) + self.get_z().powi(2) + self.get_w().powi(2))
+            as f64)
             .sqrt()
     }
 }
