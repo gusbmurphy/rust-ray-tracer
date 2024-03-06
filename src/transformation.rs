31ffca47747a7ea3435bb0b896fb6b40ad3c0ae0
Augustus Murphy <gusbmurphy@gmail.com>
2024-03-05 21:22:07 -0600

    Implement X rotation, and also do some formatting

--- src/transformation.rs
+++ src/transformation.rs
@@ -15,33 +15,38 @@ impl Transformation {
         matrix.set_value(3, 1, y);
         matrix.set_value(3, 2, z);
 
-        Transformation {
-            matrix
-        }
+        Transformation { matrix }
     }
 
     pub fn new_scaling(x: f32, y: f32, z: f32) -> Self {
         let mut matrix = IDENTITY_MATRIX;
-        
+
         matrix.set_value(0, 0, x);
         matrix.set_value(1, 1, y);
         matrix.set_value(2, 2, z);
 
-        Transformation {
-            matrix
-        }
+        Transformation { matrix }
+    }
+
+    pub fn new_x_rotation(radians: f32) -> Self {
+        let mut matrix = IDENTITY_MATRIX;
+
+        matrix.set_value(1, 1, radians.cos());
+        matrix.set_value(2, 1, -radians.sin());
+        matrix.set_value(1, 2, radians.sin());
+        matrix.set_value(2, 2, radians.cos());
+
+        Transformation { matrix }
     }
 
     pub fn invert(&self) -> Result<Transformation, &'static str> {
         let inversion_result = self.matrix.invert();
 
         match inversion_result {
-            Ok(inverted_matrix) => {
-                Ok(Transformation { matrix: inverted_matrix })
-            },
-            Err(error) => {
-                Err(error)
-            }
+            Ok(inverted_matrix) => Ok(Transformation {
+                matrix: inverted_matrix,
+            }),
+            Err(error) => Err(error),
         }
     }
 }
@@ -57,8 +62,10 @@ impl<T: Tuple> ops::Mul<T> for Transformation {
 
 #[cfg(test)]
 mod test {
-    use crate::vector::Vector;
+    use std::f32::consts::PI;
+
     use crate::point::Point;
+    use crate::vector::Vector;
 
     use super::*;
 
@@ -99,7 +106,7 @@ mod test {
     fn scaling_a_point() {
         let scaling = Transformation::new_scaling(2.0, 3.0, 4.0);
         let point = Point::new(-4.0, 6.0, 8.0);
-        
+
         let result = scaling * point;
         let expected = Point::new(-8.0, 18.0, 32.0);
 
@@ -110,7 +117,7 @@ mod test {
     fn scaling_a_vector() {
         let scaling = Transformation::new_scaling(2.0, 3.0, 4.0);
         let vector = Vector::new(-4.0, 6.0, 8.0);
-        
+
         let result = scaling * vector;
         let expected = Vector::new(-8.0, 18.0, 32.0);
 
@@ -123,7 +130,7 @@ mod test {
         let inversion = scaling.invert().unwrap();
 
         let vector = Vector::new(-4.0, 6.0, 8.0);
-        
+
         let result = inversion * vector;
         let expected = Vector::new(-2.0, 2.0, 2.0);
 
@@ -139,5 +146,32 @@ mod test {
         let expected = Point::new(-2.0, 3.0, 4.0);
 
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn rotating_a_point_around_the_x_axis() {
+        let point = Point::new(0.0, 1.0, 0.0);
+
+        let half_quarter = Transformation::new_x_rotation(PI / 4.0);
+        let full_quarter = Transformation::new_x_rotation(PI / 2.0);
+
+        assert_eq!(
+            half_quarter * point,
+            Point::new(0.0, 2.0f32.sqrt() / 2.0, 2.0f32.sqrt() / 2.0)
+        );
+        assert_eq!(full_quarter * point, Point::new(0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn inverse_of_an_x_rotation_rotates_in_opposite_direction() {
+        let point = Point::new(0.0, 1.0, 0.0);
+
+        let half_quarter = Transformation::new_x_rotation(PI / 4.0);
+        let inverse = half_quarter.invert().unwrap();
+
+        assert_eq!(
+            inverse * point,
+            Point::new(0.0, 2.0f32.sqrt() / 2.0, -2.0f32.sqrt() / 2.0)
+        );
     }
 }
