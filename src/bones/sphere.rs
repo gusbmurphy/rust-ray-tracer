7c147a45a1832065539a4bed0f6b3bb5cdff5634
Augustus Murphy <gusbmurphy@gmail.com>
2024-03-30 21:08:11 -0500

    fixup! Create Sphere

--- src/bones/sphere.rs
+++ src/bones/sphere.rs
@@ -2,14 +2,14 @@ use crate::prelude::*;
 
 pub struct Sphere {
     center: Point,
-    radius: f32
+    radius: f32,
 }
 
 impl Sphere {
     pub fn new() -> Self {
         Sphere {
             center: Point::new(0.0, 0.0, 0.0),
-            radius: 1.0
+            radius: 1.0,
         }
     }
 
