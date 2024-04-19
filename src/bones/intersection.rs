caa84606e05ff0c8991f63080545c42eae60ed51
Augustus Murphy <gusbmurphy@gmail.com>
2024-04-19 07:59:19 -0500

    Create unimplemented `determine_hit` function

--- src/bones/intersection.rs
+++ src/bones/intersection.rs
@@ -22,3 +22,31 @@ where
 }
 
 pub trait Intersectable {}
+
+pub fn determine_hit<const S: usize, T>(
+    intersections: [Intersection<T>; S],
+) -> Option<Intersection<T>>
+where
+    T: Intersectable,
+{
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use crate::bones::sphere::Sphere;
+
+    use super::*;
+
+    #[test]
+    fn the_lowest_positive_t_among_positives_is_the_hit() {
+        let interesected_sphere = Sphere::new();
+
+        let i1 = Intersection::new(1.0, &interesected_sphere);
+        let i2 = Intersection::new(2.0, &interesected_sphere);
+
+        let result = determine_hit([i1, i2]).unwrap();
+
+        assert_eq!(result, i1);
+    }
+}
