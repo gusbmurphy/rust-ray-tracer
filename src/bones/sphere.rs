bfde45ccf50aad3a21429c462a8f26bcc87a2732
Augustus Murphy <gusbmurphy@gmail.com>
2024-05-19 20:41:20 -0500

    Add `set_transform` to `Sphere`

--- src/bones/sphere.rs
+++ src/bones/sphere.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 use super::{
     intersection::Intersectable,
     matrix::{Matrix, IDENTITY_MATRIX},
+    transformation::Transformation
 };
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -28,6 +29,10 @@ impl Sphere {
     pub fn get_transform(&self) -> &Matrix<4> {
         &self.transform
     }
+
+    pub fn set_transform(&mut self, transformation: Transformation) {
+        self.transform = transformation.get_matrix().to_owned();
+    }
 }
 
 impl Intersectable for Sphere {}
@@ -37,8 +42,18 @@ mod test {
     use super::*;
 
     #[test]
-    fn default_sphere_transformation() {
+    fn default_sphere_transform() {
         let sphere = Sphere::new();
         assert_eq!(sphere.get_transform().to_owned(), IDENTITY_MATRIX);
+    }
+
+    #[test]
+    fn changing_sphere_transform() {
+        let mut sphere = Sphere::new();
+        let translation = Transformation::new_translation(2.0, 2.0, 4.0);
+
+        sphere.set_transform(translation);
+
+        assert!(translation == sphere.get_transform().to_owned() );
     }
 }
