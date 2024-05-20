84cd812cc7a60523113f18bdfd85684498f8c935
Augustus Murphy <gusbmurphy@gmail.com>
2024-05-20 08:34:47 -0500

    Small formatting change in `transformation.rs`

--- src/bones/transformation.rs
+++ src/bones/transformation.rs
@@ -135,7 +135,7 @@ impl ops::Mul<Transformation> for Transformation {
 impl PartialEq<Matrix<4>> for Transformation {
     fn eq(&self, other: &Matrix<4>) -> bool {
         self.matrix == other.to_owned()
-    }    
+    }
 }
 
 #[cfg(test)]
