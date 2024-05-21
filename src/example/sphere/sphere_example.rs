270a65d18174542c414e5af14bcd0c7148a072b7
Augustus Murphy <gusbmurphy@gmail.com>
2024-05-20 21:48:48 -0500

    Make the sphere example work

--- src/example/sphere/sphere_example.rs
+++ src/example/sphere/sphere_example.rs
@@ -6,22 +6,27 @@ use crate::{
 pub fn draw_sphere_example_ppm() -> String {
     let ray_origin = Point::new(0.0, 0.0, 3.0);
 
-    let mut sphere = Sphere::new();
-    let sphere_translation = Transformation::new_translation(0.0, 0.0, -1.0);
-    sphere.set_transform(sphere_translation);
+    let sphere = Sphere::new();
+
+    const WALL_Z: f32 = -6.0;
+    const WALL_WIDTH: f32 = 6.0;
 
     const CANVAS_SIZE: u64 = 300;
     let mut canvas = Canvas::new(CANVAS_SIZE, CANVAS_SIZE);
 
-    for x in 1..=CANVAS_SIZE {
-        for y in 1..=CANVAS_SIZE {
-            let ray = Ray::new(ray_origin, Vector::new(x as f32, y as f32, -1.0));
+    for canvas_x in 0..CANVAS_SIZE {
+        let wall_x: f32 = (canvas_x as f32 / CANVAS_SIZE as f32) * WALL_WIDTH - (WALL_WIDTH * 0.5);
+
+        for canvas_y in 0..CANVAS_SIZE {
+            let wall_y: f32 = (canvas_y as f32 / CANVAS_SIZE as f32) * WALL_WIDTH - (WALL_WIDTH * 0.5);
+
+            let ray = Ray::new(ray_origin, Vector::new(wall_x, wall_y, WALL_Z));
 
             let intersections = ray.intersections_with(&sphere);
 
             let color = if intersections.is_some() { RED } else { BLACK };
 
-            canvas.write_pixel(x as usize - 1, y as usize - 1, color);
+            canvas.write_pixel(canvas_x as usize, canvas_y as usize, color);
         }
     }
 
