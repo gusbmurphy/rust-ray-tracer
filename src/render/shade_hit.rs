afd7ded3a936a0bc717fdaa9a245ce23f814788e
Augustus Murphy <gusbmurphy@gmail.com>
2024-05-29 22:22:42 -0500

    Add test for shading hit from inside

--- src/render/shade_hit.rs
+++ src/render/shade_hit.rs
@@ -9,7 +9,8 @@ pub fn shade_hit(world: &World, precomputation: &Precomputation<Sphere>) -> Colo
     );
 
     return lighting_calculator.get_color_for_material_at(
-        *precomputation.get_object().get_material(), precomputation.get_hit_point()
+        *precomputation.get_object().get_material(),
+        precomputation.get_hit_point(),
     );
 }
 
@@ -30,5 +31,24 @@ mod test {
         let result = shade_hit(&world, &precomputation);
 
         assert_eq!(result, Color::new(0.38066, 0.47583, 0.2855))
+    }
+
+    #[test]
+    fn shading_an_intersection_from_the_inside() {
+        let mut world = World::get_default();
+
+        let light = PointLight::new(Color::new(1.0, 1.0, 1.0), Point::new(0.0, 0.25, 0.0));
+        world.set_light(light);
+
+        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), Vector::new(0.0, 0.0, 1.0));
+
+        let hit_object = world.get_object(1).unwrap();
+        let intersection = Intersection::new(0.5, hit_object);
+
+        let precomputation = Precomputation::new(&intersection, &ray);
+
+        let result = shade_hit(&world, &precomputation);
+
+        assert_eq!(result, Color::new(0.90498, 0.90498, 0.90498))
     }
 }
