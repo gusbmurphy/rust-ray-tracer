6e2bb67704778cf0dbde47292b38352238e7a85c
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-08 20:53:58 -0500

    Add function to get pixel size

--- src/render/camera.rs
+++ src/render/camera.rs
@@ -16,6 +16,38 @@ impl Camera {
             transform: Transform::new(IDENTITY_MATRIX)
         }
     }
+
+    pub fn get_pixel_size(&self) -> f32 {
+        (self.get_half_width() * 2.0) / (self.horizontal_size as f32)
+    }
+
+    fn get_half_view(&self) -> f32 {
+        (self.field_of_view / 2.0).tan()
+    }
+
+    fn get_half_width(&self) -> f32 {
+        let aspect = self.get_aspect();
+
+        if aspect >= 1.0 {
+            return self.get_half_view();
+        }
+
+        return self.get_half_view() * aspect;
+    }
+
+    fn get_half_height(&self) -> f32 {
+        let aspect: f32 = self.get_aspect();
+
+        if aspect >= 1.0 {
+            return self.get_half_view() / aspect;
+        }
+
+        return self.get_half_view();
+    }
+
+    fn get_aspect(&self) -> f32 {
+        (self.horizontal_size as f32) / (self.vertical_size as f32)
+    }
 }
 
 #[cfg(test)]
@@ -29,5 +61,19 @@ mod test {
         let camera = Camera::new(160, 120, PI / 2.0);
 
         assert_eq!(camera.transform, IDENTITY_MATRIX);
+    }
+
+    #[test]
+    fn the_pixel_size_for_a_horizontal_canvas_is_correct() {
+        let camera = Camera::new(200, 125, PI / 2.0);
+
+        assert_eq!(camera.get_pixel_size(), 0.01);
+    }
+
+    #[test]
+    fn the_pixel_size_for_a_vertical_canvas_is_correct() {
+        let camera = Camera::new(125, 200, PI / 2.0);
+
+        assert_eq!(camera.get_pixel_size(), 0.01);
     }
 }
