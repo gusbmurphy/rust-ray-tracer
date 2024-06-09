46b1efd92a937db51ea1d01b62dab4931ad9861d
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-08 21:33:08 -0500

    Add tests and `todo` implementation of `get_ray_for_pixel`

--- src/render/camera.rs
+++ src/render/camera.rs
@@ -13,12 +13,30 @@ impl Camera {
             horizontal_size,
             vertical_size,
             field_of_view,
-            transform: Transform::new(IDENTITY_MATRIX)
+            transform: Transform::new(IDENTITY_MATRIX),
+        }
+    }
+
+    pub fn new_with_transform(
+        horizontal_size: u32,
+        vertical_size: u32,
+        field_of_view: f32,
+        transform: Transform,
+    ) -> Self {
+        Camera {
+            horizontal_size,
+            vertical_size,
+            field_of_view,
+            transform,
         }
     }
 
     pub fn get_pixel_size(&self) -> f32 {
         (self.get_half_width() * 2.0) / (self.horizontal_size as f32)
+    }
+    
+    pub fn get_ray_for_pixel(&self, pixel_x: u32, pixel_y: u32) -> Ray {
+        todo!()
     }
 
     fn get_half_view(&self) -> f32 {
@@ -75,5 +93,40 @@ mod test {
         let camera = Camera::new(125, 200, PI / 2.0);
 
         assert_eq!(camera.get_pixel_size(), 0.01);
+    }
+
+    #[test]
+    fn constructing_a_ray_through_the_center_of_the_canvas() {
+        let camera = Camera::new(201, 101, PI / 2.0);
+
+        let ray: Ray = camera.get_ray_for_pixel(100, 50);
+
+        assert_eq!(ray.get_origin().to_owned(), Point::new(0.0, 0.0, 0.0));
+        assert_eq!(ray.get_direction().to_owned(), Vector::new(0.0, 0.0, -1.0));
+    }
+
+    #[test]
+    fn constructing_a_ray_through_a_corner_of_the_canvas() {
+        let camera = Camera::new(201, 101, PI / 2.0);
+
+        let ray: Ray = camera.get_ray_for_pixel(0, 0);
+
+        assert_eq!(ray.get_origin().to_owned(), Point::new(0.0, 0.0, 0.0));
+        assert_eq!(ray.get_direction().to_owned(), Vector::new(0.66519, 0.33259, -0.66851));
+    }
+
+    #[test]
+    fn constructing_a_ray_when_the_camera_is_transformed() {
+        let transform =
+            Transform::new_y_rotation(PI / 4.0) * Transform::new_translation(0.0, -2.0, 5.0);
+        let camera = Camera::new_with_transform(201, 101, PI / 2.0, transform);
+
+        let ray: Ray = camera.get_ray_for_pixel(100, 50);
+
+        assert_eq!(ray.get_origin().to_owned(), Point::new(0.0, 2.0, -5.0));
+        assert_eq!(
+            ray.get_direction().to_owned(),
+            Vector::new(2.0f32.sqrt() / 2.0, 0.0, -2.0f32.sqrt() / 2.0)
+        );
     }
 }
