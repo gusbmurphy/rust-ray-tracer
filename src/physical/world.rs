4b770d425b97baf0f434638198ee2415e2ef1ae1
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-17 14:21:18 -0500

    fixup! Add some tests to figure out what's happening with the shadows...

--- src/physical/world.rs
+++ src/physical/world.rs
@@ -202,7 +202,7 @@ mod test {
         let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
 
         // The intersection will happen with the outermost sphere...
-        let intersection = Intersection::new(4.5, world.get_object(0).unwrap());
+        let intersection = Intersection::new(4.0, world.get_object(0).unwrap());
         let computation = Precomputation::new(&intersection, &ray);
 
         let is_point_shadowed = world.is_point_shadowed(&computation.get_adjusted_hit_point());
