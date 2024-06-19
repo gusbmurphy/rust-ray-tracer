93acff9d85d82a14213fac4b8e925d7d07a89ec2
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-19 07:44:00 -0500

    Some formatting

--- src/geometry/tuple.rs
+++ src/geometry/tuple.rs
@@ -7,9 +7,7 @@ pub trait Tuple {
     fn new(x: f32, y: f32, z: f32) -> Self;
 
     fn magnitude(&self) -> f32 {
-        ((self.x().powi(2) + self.y().powi(2) + self.z().powi(2) + self.w().powi(2))
-            as f32)
-            .sqrt()
+        ((self.x().powi(2) + self.y().powi(2) + self.z().powi(2) + self.w().powi(2)) as f32).sqrt()
     }
 
     fn to_array(&self) -> [f32; 4] {
