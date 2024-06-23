71cdded088595f8642dc2d147292a9a21c49b487
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-22 22:01:22 -0500

    fixup! Add `intersections_with` to `Shape`

--- src/geometry/shape.rs
+++ src/geometry/shape.rs
@@ -2,5 +2,7 @@ use crate::prelude::*;
 
 pub trait Shape: Sized {
     fn normal_at(&self, world_space_point: Point) -> Vector;
-    fn intersections_with<'s, 'r>(&'s self, ray: &'r Ray) -> Vec<Intersection<Self>> where 'r: 's;
+    fn intersections_with<'s, 'r>(&'s self, ray: &'r Ray) -> Vec<Intersection<Self>>
+    where
+        'r: 's;
 }
