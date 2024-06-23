3b87981e2c6cca1f7c41446105eee12abd0c6482
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-22 22:11:02 -0500

    Remove `cast_into` and `intersections_with` from `Ray`

--- src/geometry/ray.rs
+++ src/geometry/ray.rs
@@ -1,8 +1,5 @@
 use crate::prelude::*;
 
-use super::intersection::Intersection;
-use super::vector::dot;
-
 #[derive(PartialEq, Debug)]
 pub struct Ray {
     origin: Point,
@@ -25,65 +22,6 @@ impl Ray {
     pub fn position_at(&self, time: f32) -> Point {
         self.origin + self.direction * time
     }
-
-    pub fn cast_into<'r, 'w>(&'r self, world: &'w World) -> Option<Intersection<Sphere>>
-    where
-        'w: 'r,
-    {
-        let mut intersections = Vec::new();
-
-        for object in world.objects().as_slice() {
-            if let Some(actual_intersections) = self.intersections_with(&object) {
-                intersections.extend(Vec::from(actual_intersections));
-            }
-        }
-
-        let mut lowest_t_intersection: Option<Intersection<Sphere>> = None;
-
-        for intersection in intersections {
-            if *intersection.t() > 0f32 {
-                match lowest_t_intersection {
-                    None => lowest_t_intersection = Some(intersection),
-                    Some(ref lowest_t) => {
-                        if intersection.t() < lowest_t.t() {
-                            lowest_t_intersection = Some(intersection);
-                        }
-                    }
-                }
-            }
-        }
-
-        return lowest_t_intersection;
-    }
-
-    pub fn intersections_with<'a, 'b>(
-        &'a self,
-        sphere: &'b Sphere,
-    ) -> Option<[Intersection<Sphere>; 2]>
-    where
-        'b: 'a,
-    {
-        let adjusted_ray = sphere.transform().invert().unwrap() * self;
-        let vector_from_sphere_to_ray = *adjusted_ray.origin() - *sphere.center();
-
-        let a = dot(adjusted_ray.direction(), adjusted_ray.direction());
-        let b = 2f32 * dot(adjusted_ray.direction(), &vector_from_sphere_to_ray);
-        let c = dot(&vector_from_sphere_to_ray, &vector_from_sphere_to_ray) - 1f32;
-
-        let discriminant = b.powi(2) - 4f32 * a * c;
-
-        if discriminant < 0f32 {
-            return None;
-        }
-
-        let t1 = (-b - discriminant.sqrt()) / (2f32 * a);
-        let t2 = (-b + discriminant.sqrt()) / (2f32 * a);
-
-        return Some([
-            Intersection::new(t1, sphere, &self),
-            Intersection::new(t2, sphere, &self),
-        ]);
-    }
 }
 
 #[cfg(test)]
@@ -98,78 +36,5 @@ mod test {
         assert_eq!(ray.position_at(1.0), Point::new(3.0, 3.0, 4.0));
         assert_eq!(ray.position_at(-1.0), Point::new(1.0, 3.0, 4.0));
         assert_eq!(ray.position_at(2.5), Point::new(4.5, 3.0, 4.0));
-    }
-
-    #[test]
-    fn intersection_through_middle_of_sphere() {
-        let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
-        let sphere = Sphere::new();
-
-        let intersections = ray.intersections_with(&sphere);
-
-        for intersection in intersections.unwrap() {
-            assert_eq!(*intersection.intersected_object(), sphere);
-        }
-
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == 4.0);
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == 6.0);
-    }
-
-    #[test]
-    fn ray_missing_a_sphere() {
-        let ray = Ray::new(Point::new(0.0, 2.0, -5.0), Vector::new(0.0, 0.0, 1.0));
-        let sphere = Sphere::new();
-
-        let intersections = ray.intersections_with(&sphere);
-
-        assert_eq!(intersections, None);
-    }
-
-    #[test]
-    fn ray_originating_inside_of_a_sphere() {
-        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), Vector::new(0.0, 0.0, 1.0));
-        let sphere = Sphere::new();
-
-        let intersections = ray.intersections_with(&sphere);
-
-        for intersection in intersections.unwrap() {
-            assert_eq!(*intersection.intersected_object(), sphere);
-        }
-
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == -1.0);
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == 1.0);
-    }
-
-    #[test]
-    fn ray_ahead_of_a_sphere() {
-        let ray = Ray::new(Point::new(0.0, 0.0, 5.0), Vector::new(0.0, 0.0, 1.0));
-        let sphere = Sphere::new();
-
-        let intersections = ray.intersections_with(&sphere);
-
-        for intersection in intersections.as_ref().unwrap() {
-            assert_eq!(*intersection.intersected_object(), sphere);
-        }
-
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == -6.0);
-        intersections
-            .unwrap()
-            .iter()
-            .any(|intersection| *intersection.t() == -4.0);
     }
 }
