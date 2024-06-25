1e54a0916d3ec3ec1866435b04d22393bc4e1869
Augustus Murphy <gusbmurphy@gmail.com>
2024-06-25 07:59:43 -0500

    Remove `point` and `normal` from `Plane` to use transformations correctly

--- src/geometry/plane.rs
+++ src/geometry/plane.rs
@@ -2,17 +2,13 @@ use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Plane {
-    point: Point,
-    normal: Vector,
     transform: Transform,
     material: Material,
 }
 
 impl Plane {
-    pub fn new(point: Point, normal: Vector) -> Self {
+    pub fn new() -> Self {
         Plane {
-            point,
-            normal,
             transform: Transform::new(IDENTITY_MATRIX),
             material: Material::new(),
         }
@@ -21,18 +17,20 @@ impl Plane {
 
 impl Shape for Plane {
     fn normal_at(&self, _world_space_point: Point) -> Vector {
-        self.normal.clone()
+        self.transform * POSITIVE_Y
     }
 
     fn intersections_with<'s, 'r>(&'s self, ray: &'r Ray) -> Vec<Intersection>
     where
         'r: 's,
     {
-        if ray.direction().y().abs() < EPSILON {
+        let local_ray = self.transform.invert().unwrap() * ray;
+
+        if local_ray.direction().y().abs() < EPSILON {
             return vec![];
         }
 
-        let t = -ray.origin().y() / ray.direction().y();
+        let t = -local_ray.origin().y() / local_ray.direction().y();
         let intersection = Intersection::new(t, Box::new(*self), &ray);
 
         vec![intersection]
@@ -57,11 +55,13 @@ impl Shape for Plane {
 
 #[cfg(test)]
 mod test {
+    use std::f32::consts::PI;
+
     use super::*;
 
     #[test]
     fn the_normal_vector_is_always_the_same() {
-        let plane = Plane::new(ORIGIN, POSITIVE_Y);
+        let plane = Plane::new();
 
         assert_eq!(plane.normal_at(ORIGIN), POSITIVE_Y);
         assert_eq!(plane.normal_at(Point::new(1.0, 0.0, 0.0)), POSITIVE_Y);
@@ -71,7 +71,7 @@ mod test {
     #[test]
     fn there_are_no_intersections_with_a_parallel_ray() {
         // Given a flat plane, and a ray above it running parallel...
-        let plane = Plane::new(ORIGIN, POSITIVE_Y);
+        let plane = Plane::new();
         let ray = Ray::new(Point::new(0.0, 1.0, 0.0), POSITIVE_Z);
 
         let intersections = plane.intersections_with(&ray);
@@ -82,7 +82,7 @@ mod test {
     #[test]
     fn there_are_no_intersections_with_a_coplanar_ray() {
         // Given a flat plane, and a ray at the same height running parallel...
-        let plane = Plane::new(ORIGIN, POSITIVE_Y);
+        let plane = Plane::new();
         let ray = Ray::new(Point::new(0.0, 0.0, 0.0), POSITIVE_Z);
 
         let intersections = plane.intersections_with(&ray);
@@ -92,7 +92,7 @@ mod test {
 
     #[test]
     fn a_ray_intersection_from_above() {
-        let plane = Plane::new(ORIGIN, POSITIVE_Y);
+        let plane = Plane::new();
         let ray = Ray::new(Point::new(0.0, 3.0, 0.0), NEGATIVE_Y);
 
         let intersections = plane.intersections_with(&ray);
@@ -105,7 +105,7 @@ mod test {
 
     #[test]
     fn a_ray_intersection_from_below() {
-        let plane = Plane::new(ORIGIN, POSITIVE_Y);
+        let plane = Plane::new();
         let ray = Ray::new(Point::new(0.0, -3.0, 0.0), POSITIVE_Y);
 
         let intersections = plane.intersections_with(&ray);
@@ -117,15 +117,17 @@ mod test {
     }
 
     #[test]
-    fn an_intersection_with_a_plane_at_an_angle() {
-        let plane = Plane::new(ORIGIN, Vector::new(0.0, 1.0, 1.0));
-        let ray = Ray::new(Point::new(0.0, 1.0, 1.0), Vector::new(0.0, -1.0, -1.0));
+    fn intersection_when_plane_is_up_and_down() {
+        let mut plane = Plane::new();
+        plane.set_transform(Transform::new_translation(0.0, 0.0, 9.0) * Transform::new_x_rotation(PI/2.0));
+
+        let ray = Ray::new(ORIGIN, POSITIVE_Z);
 
         let intersections = plane.intersections_with(&ray);
 
         assert_eq!(intersections.len(), 1);
 
         let intersection = intersections.get(0).unwrap();
-        assert_eq!(*intersection.t(), 1.0);
+        assert_eq!(*intersection.t(), 9.0);
     }
 }
