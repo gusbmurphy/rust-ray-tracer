889ae1432e739f9d01150958f7a6354e85f2e952
Augustus Murphy <gusbmurphy@gmail.com>
2024-07-01 16:48:26 -0400

    Make `Shape` inherit from `Any`

--- src/geometry/shape.rs
+++ src/geometry/shape.rs
@@ -1,6 +1,8 @@
+use std::any::Any;
+
 use crate::prelude::*;
 
-pub trait Shape {
+pub trait Shape: Any {
     fn normal_at(&self, world_space_point: Point) -> Vector;
     fn intersections_with<'s, 'r>(&'s self, ray: &'r Ray) -> Vec<Intersection>
     where
