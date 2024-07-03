dd3e9a4e35877219e167588acae42111e87dd4d7
Augustus Murphy <gusbmurphy@gmail.com>
2024-07-03 18:44:59 -0400

    Change `main` to look for CLI

--- src/main.rs
+++ src/main.rs
@@ -10,48 +10,22 @@ mod prelude {
     pub use crate::render::*;
 }
 
-use example::draw_scene_ppm;
+use crate::prelude::create_ppm_from_canvas;
+use parse::parse_scene_from_yaml;
+use std::{error::Error, fs::File, io::Write};
 
-use crate::example::draw_clock_example_ppm;
-use crate::example::draw_projectile_example_ppm;
-use std::fs::File;
-use std::io::prelude::*;
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = std::env::args().nth(1).expect("No file path given");
+    let target = std::env::args().nth(2).expect("No target name given");
 
-fn main() -> Result<(), std::io::Error> {
-    let projectile_result = draw_projectile_example_to_file();
-    let clock_result = draw_clock_example_to_file();
-    let scene_result = draw_scene_example_to_file();
+    let (world, camera) = parse_scene_from_yaml(&path)?;
 
-    if projectile_result.is_ok() {
-        return clock_result;
-    } else {
-        return projectile_result;
-    }
-}
+    let ppm = create_ppm_from_canvas(camera.render(world));
 
-fn draw_clock_example_to_file() -> std::io::Result<()> {
-    let ppm_data = draw_clock_example_ppm(100, 25);
+    let mut file = File::create(format!("{}.ppm", target))?;
+    file.write_all(ppm.as_bytes())?;
 
-    let mut file = File::create("clock.ppm")?;
-    file.write_all(ppm_data.as_bytes())?;
-
-    Ok(())
-}
-
-fn draw_projectile_example_to_file() -> std::io::Result<()> {
-    let ppm_data = draw_projectile_example_ppm();
-
-    let mut file = File::create("projectile.ppm")?;
-    file.write_all(ppm_data.as_bytes())?;
-
-    Ok(())
-}
-
-fn draw_scene_example_to_file() -> std::io::Result<()> {
-    let ppm_data = draw_scene_ppm(1000, 500);
-
-    let mut file = File::create("scene.ppm")?;
-    file.write_all(ppm_data.as_bytes())?;
+    println!("All done! Created a new file: {}.ppm", target);
 
     Ok(())
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -3,3 +3,5 @@ mod parse_light;
 mod parse_little_things;
 mod parse_shape;
 mod parse_yaml;
+
+pub use parse_yaml::parse_scene_from_yaml;
