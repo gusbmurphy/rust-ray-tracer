ff688d3a6a8ffffa246e934264ed6ff4ae48d6f6
Augustus Murphy <gusbmurphy@gmail.com>
2024-07-09 09:37:05 -0400

    Allow scaling checkered pattern

--- src/pattern/checkers.rs
+++ src/pattern/checkers.rs
@@ -6,6 +6,7 @@ use super::pattern;
 struct CheckerPattern {
     background: Color,
     checker: Color,
+    transform: Transform,
 }
 
 impl CheckerPattern {
@@ -13,14 +14,20 @@ impl CheckerPattern {
         CheckerPattern {
             background,
             checker,
+            transform: Transform::new(IDENTITY_MATRIX),
         }
+    }
+
+    pub fn set_transform(&mut self, transform: Transform) {
+        self.transform = transform;
     }
 }
 
 impl Pattern for CheckerPattern {
     fn color_at(&self, point: &Point) -> Color {
-        let x = point.x();
-        let z = point.z();
+        let pattern_space_point = self.transform.invert().unwrap() * *point;
+        let x = pattern_space_point.x();
+        let z = pattern_space_point.z();
 
         let x_is_in_background = x.floor().rem_euclid(2.0) == 0.0;
         let z_is_in_background = z.floor().rem_euclid(2.0) == 0.0;
@@ -62,5 +69,23 @@ mod test {
 
         assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.0)), WHITE);
         assert_eq!(pattern.color_at(&Point::new(0.0, 1.0, 0.0)), WHITE);
+    }
+
+    #[test]
+    fn the_pattern_can_be_scaled() {
+        let mut pattern = CheckerPattern::new(WHITE, BLACK);
+        pattern.set_transform(Transform::scaling(0.5, 0.5, 0.5));
+
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.0)), WHITE);
+        assert_eq!(pattern.color_at(&Point::new(0.4, 0.0, 0.0)), WHITE);
+        assert_eq!(pattern.color_at(&Point::new(0.5, 0.0, 0.0)), BLACK);
+        assert_eq!(pattern.color_at(&Point::new(0.9, 0.0, 0.0)), BLACK);
+        assert_eq!(pattern.color_at(&Point::new(1.0, 0.0, 0.0)), WHITE);
+
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.0)), WHITE);
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.4)), WHITE);
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.5)), BLACK);
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 0.9)), BLACK);
+        assert_eq!(pattern.color_at(&Point::new(0.0, 0.0, 1.0)), WHITE);
     }
 }
