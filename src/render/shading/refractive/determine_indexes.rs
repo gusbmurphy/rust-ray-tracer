9114b7ab8402f8fc5615ba96d711261fdc815158
Augustus Murphy <gusbmurphy@gmail.com>
2024-09-22 22:45:56 -0500

    Determine indexes for internal sphere

--- src/render/shading/refractive/determine_indexes.rs
+++ src/render/shading/refractive/determine_indexes.rs
@@ -1,11 +1,32 @@
 use crate::prelude::*;
+use std::rc::Rc;
 
 /// Determines the refractive indexes of the materials exited and entered (in that order) given a number of `Intersection`s. If there is no material exited, or one not entered, a value of `1.0` will be given.
-pub fn determine_refractive_indexes(intersections: Vec<Intersection>) -> [f64; 2] {
-    [
-        1.0,
-        *intersections.get(0).unwrap().material().refractive_index(),
-    ]
+pub fn determine_refractive_indexes(intersections: &Vec<Intersection>) -> [f64; 2] {
+    let mut exited_ri = 1.0;
+    let mut entered_ri = 1.0;
+
+    let hit = determine_hit(intersections.to_owned().to_vec());
+    let mut shapes_entered: Vec<Rc<dyn Shape>> = Vec::new();
+
+    for (index, intersection) in intersections.iter().enumerate() {
+        shapes_entered.push(intersection.object());
+
+        if let Some(ref h) = hit {
+            if h == intersection {
+                entered_ri = *intersection.object().material().refractive_index();
+
+                // A bit of a yikes here...
+                if let Some(last_index) = index.checked_sub(1) {
+                    if let Some(last_object_exited) = shapes_entered.get(last_index) {
+                        exited_ri = *last_object_exited.material().refractive_index();
+                    }
+                }
+            }
+        }
+    }
+
+    [exited_ri, entered_ri]
 }
 
 #[cfg(test)]
@@ -21,7 +42,7 @@ mod test {
         let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
         let intersections = Intersection::of(&sphere, &ray);
 
-        let result = determine_refractive_indexes(intersections);
+        let result = determine_refractive_indexes(&intersections);
         assert_eq!(result[0], 1.0); // 1.0 since there is no material being exited.
         assert_eq!(result[1], 3.0);
     }
@@ -41,7 +62,7 @@ mod test {
         let mut intersections = Intersection::of(&outer_rc, &ray);
         intersections.append(&mut Intersection::of(&inner_rc, &ray));
 
-        let result = determine_refractive_indexes(intersections);
+        let result = determine_refractive_indexes(&intersections);
         assert_eq!(result[0], 3.0);
         assert_eq!(result[1], 9.7);
     }
