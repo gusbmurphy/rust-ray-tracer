e9dc39feae8065a75aee9d32949bc8409ef329f2
Augustus Murphy <gusbmurphy@gmail.com>
2024-09-22 22:02:52 -0500

    Add test for existing a sphere and entering an internal one

--- src/render/shading/refractive/determine_indexes.rs
+++ src/render/shading/refractive/determine_indexes.rs
@@ -25,4 +25,24 @@ mod test {
         assert_eq!(result[0], 1.0); // 1.0 since there is no material being exited.
         assert_eq!(result[1], 3.0);
     }
+
+    #[test]
+    fn exiting_a_sphere_and_entering_an_internal_one() {
+        let mut outer =
+            Sphere::new_with_material(MaterialBuilder::new().refractive_index(3.0).build());
+        outer.set_transform(Transform::scaling(3.0, 3.0, 3.0));
+
+        let inner = Sphere::new_with_material(MaterialBuilder::new().refractive_index(9.7).build());
+
+        let ray = Ray::new(Point::new(0.0, 0.0, -1.5), Vector::new(0.0, 0.0, 1.0));
+        let outer_rc = Rc::new(outer) as Rc<dyn Shape>;
+        let inner_rc = Rc::new(inner) as Rc<dyn Shape>;
+
+        let mut intersections = Intersection::of(&outer_rc, &ray);
+        intersections.append(&mut Intersection::of(&inner_rc, &ray));
+
+        let result = determine_refractive_indexes(intersections);
+        assert_eq!(result[0], 3.0);
+        assert_eq!(result[1], 9.7);
+    }
 }
