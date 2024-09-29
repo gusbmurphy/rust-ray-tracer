8353f41732252aa6b70721994755f659dd0b7968
Augustus Murphy <gusbmurphy@gmail.com>
2024-09-28 19:31:22 -0500

    fixup! Add `adjust_hit_under` function

--- src/render/shading/ambient.rs
+++ src/render/shading/ambient.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
-use crate::render::shading::shade_ray::adjust_hit;
+use crate::render::shading::shade_ray::adjust_hit_over;
 
 pub fn calculate_ambient_contribution(light: &PointLight, hit: &Intersection) -> Color {
-    let adjusted_hit = adjust_hit(&hit);
+    let adjusted_hit = adjust_hit_over(&hit);
     let hit_in_object_space = hit.object().transform().invert().unwrap() * adjusted_hit;
 
     let effective_color = hit.material().color_at(&hit_in_object_space) * *light.intensity();
