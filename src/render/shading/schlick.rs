5bc32c23667b8336664591f4ac00113bdc45536d
Augustus Murphy <gusbmurphy@gmail.com>
2024-09-28 22:25:36 -0500

    Fully implement `schlick_approximation`

--- src/render/shading/schlick.rs
+++ src/render/shading/schlick.rs
@@ -5,7 +5,7 @@ use super::refractive::determine_refractive_indexes;
 pub fn schlick_approximation(intersection: &Intersection) -> f64 {
     let eye_vector = -intersection.ray().direction().to_owned();
     let normal = intersection.normal_vector();
-    let cos = dot(&eye_vector, &normal);
+    let mut cos = dot(&eye_vector, &normal);
 
     let all_intersections = Intersection::of(intersection.object(), intersection.ray());
 
@@ -17,9 +17,15 @@ pub fn schlick_approximation(intersection: &Intersection) -> f64 {
         if sin2_t > 1.0 {
             return 1.0;
         }
+
+        cos = (1.0 - sin2_t).sqrt();
     }
 
-    todo!()
+    let r0 = ((refractive_indexes[0] - refractive_indexes[1])
+        / (refractive_indexes[0] + refractive_indexes[1]))
+        .powi(2);
+
+    return r0 + (1.0 - r0) * (1.0 - cos).powi(5);
 }
 
 #[cfg(test)]
@@ -42,5 +48,45 @@ mod test {
         let target_intersection = intersections.get(1).unwrap();
 
         assert_eq!(schlick_approximation(target_intersection), 1.0);
+    }
+
+    #[test]
+    fn relfectance_of_perpindicular_ray() {
+        let sphere = Rc::new(Sphere::new_with_material(
+            MaterialBuilder::new()
+                .transparency(1.0)
+                .refractive_index(1.5)
+                .build(),
+        )) as Rc<dyn Shape>;
+
+        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), POSITIVE_Y);
+
+        let intersections = Intersection::of(&sphere, &ray);
+        let target_intersection = intersections.get(1).unwrap();
+
+        assert!(close_enough(
+            &schlick_approximation(target_intersection),
+            &0.04
+        ));
+    }
+
+    #[test]
+    fn relfectance_is_high_when_the_surface_is_struck_at_a_small_angle() {
+        let sphere = Rc::new(Sphere::new_with_material(
+            MaterialBuilder::new()
+                .transparency(1.0)
+                .refractive_index(1.5)
+                .build(),
+        )) as Rc<dyn Shape>;
+
+        let ray = Ray::new(Point::new(0.0, 0.99, -2.0), POSITIVE_Z);
+
+        let intersections = Intersection::of(&sphere, &ray);
+        let target_intersection = intersections.get(0).unwrap();
+
+        assert!(close_enough(
+            &schlick_approximation(target_intersection),
+            &0.48881
+        ));
     }
 }
