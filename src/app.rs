f13ebf54d2d7d8e5d60d10ff10884474293d6ab3
Augustus Murphy <gusbmurphy@gmail.com>
2024-10-06 20:41:56 -0500

    Add color picker

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,27 @@
 use eframe::App;
+use egui::DragValue;
+use egui::UiBuilder;
 
-pub struct SceneBuilder {}
+pub struct SceneBuilder {
+    color: [f32; 3],
+}
 
 impl Default for SceneBuilder {
     fn default() -> Self {
-        Self {}
+        Self {
+            color: [0.1, 0.1, 0.1],
+        }
     }
 }
 
 impl SceneBuilder {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
 }
 
 impl App for SceneBuilder {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
@@ -26,6 +32,23 @@ impl App for SceneBuilder {
                 ui.add_space(16.0);
 
                 egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("Scene Builder");
+
+            ui.scope_builder(UiBuilder::new(), |ui| {
+                egui::Grid::new("my_grid")
+                    .num_columns(2)
+                    .spacing([40.0, 4.0])
+                    .striped(true)
+                    .show(ui, |ui| {
+                        ui.label("Color");
+                        ui.color_edit_button_rgb(&mut self.color);
+                        ui.end_row();
+                    });
             });
         });
     }
