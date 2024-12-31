edfa18465db39662c7a2226eb9d60c06d1fd696f
Augustus Murphy <gusbmurphy@gmail.com>
2024-12-30 23:53:44 -0600

    Go back to using files in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use ray_tracer::ui::SceneBuilder;
+use ray_tracer::render::render_from_yaml;
+use std::error::Error;
 
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = std::env::args().nth(1).expect("No file path given");
+    let target = std::env::args().nth(2).expect("No target name given");
 
-    let native_options = eframe::NativeOptions {
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Trace Rayer",
-        native_options,
-        Box::new(|cc| {
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::new(SceneBuilder::new(cc)))
-        }),
-    )
+    render_from_yaml(&path, &target)?;
+
+    println!("All done! Created a new file: {}.ppm", target);
+
+    Ok(())
 }
