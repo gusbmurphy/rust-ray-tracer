03022b60f617cbfc5389c8a73823587c4d51b385
Augustus Murphy <gusbmurphy@gmail.com>
2025-05-04 19:04:03 -0500

    Move `render_from_yaml` into `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use ray_tracer::render::render_from_yaml;
-use std::error::Error;
+use ray_tracer::parse::parse_scene_from_yaml;
+use ray_tracer::render::create_ppm_from_canvas;
+use std::{error::Error, fs::File, io::Write};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = std::env::args().nth(1).expect("No file path given");
@@ -8,6 +9,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     render_from_yaml(&path, &target)?;
 
     println!("All done! Created a new file: {}.ppm", target);
+
+    Ok(())
+}
+
+pub fn render_from_yaml(yaml_path: &str, target_path: &str) -> Result<(), Box<dyn Error>> {
+    let (world, camera) = parse_scene_from_yaml(yaml_path)?;
+
+    let ppm = create_ppm_from_canvas(camera.render(world));
+
+    let mut file = File::create(format!("{}.ppm", target_path))?;
+    file.write_all(ppm.as_bytes())?;
 
     Ok(())
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -3,7 +3,6 @@ pub mod canvas;
 pub mod color;
 mod create_png;
 pub mod ppm;
-mod render_from_yaml;
 mod shading;
 
 pub use camera::Camera;
@@ -11,4 +10,3 @@ pub use canvas::*;
 pub use color::*;
 pub use create_png::create_png;
 pub use ppm::*;
-pub use render_from_yaml::render_from_yaml;
