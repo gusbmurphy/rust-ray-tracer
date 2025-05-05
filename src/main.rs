080698d66e337cc2526dd1cc62030aefa447652e
Augustus Murphy <gusbmurphy@gmail.com>
2025-05-04 19:17:09 -0500

    Add progress listener

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use ray_tracer::parse::parse_scene_from_yaml;
 use ray_tracer::render::create_ppm_from_canvas;
+use ray_tracer::render::RenderProgressListener;
 use std::{error::Error, fs::File, io::Write};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = std::env::args().nth(1).expect("No file path given");
     let target = std::env::args().nth(2).expect("No target name given");
 
-    let (world, camera) = parse_scene_from_yaml(&path)?;
+    let (world, mut camera) = parse_scene_from_yaml(&path)?;
+    let listener = ProgressListener::default();
+    camera.subscribe_to_progress(&listener);
 
     let ppm = create_ppm_from_canvas(camera.render(world));
 
@@ -16,4 +19,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("All done! Created a new file: {}.ppm", target);
 
     Ok(())
+}
+
+#[derive(Default)]
+struct ProgressListener;
+
+impl RenderProgressListener for ProgressListener {
+    fn on_progress(&self, completion: f64) {
+        println!("{}% complete...", completion)
+    }
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -6,6 +6,7 @@ pub mod ppm;
 mod shading;
 
 pub use camera::Camera;
+pub use camera::RenderProgressListener;
 pub use canvas::*;
 pub use color::*;
 pub use create_png::create_png;
