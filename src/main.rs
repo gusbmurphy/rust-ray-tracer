058883117d366a0ac28921a3d183409235f61182
Augustus Murphy <gusbmurphy@gmail.com>
2025-05-04 19:05:36 -0500

    Inline `render_from_yaml`

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = std::env::args().nth(1).expect("No file path given");
     let target = std::env::args().nth(2).expect("No target name given");
 
-    render_from_yaml(&path, &target)?;
-
-    println!("All done! Created a new file: {}.ppm", target);
-
-    Ok(())
-}
-
-pub fn render_from_yaml(yaml_path: &str, target_path: &str) -> Result<(), Box<dyn Error>> {
-    let (world, camera) = parse_scene_from_yaml(yaml_path)?;
+    let (world, camera) = parse_scene_from_yaml(&path)?;
 
     let ppm = create_ppm_from_canvas(camera.render(world));
 
-    let mut file = File::create(format!("{}.ppm", target_path))?;
+    let mut file = File::create(format!("{}.ppm", target))?;
     file.write_all(ppm.as_bytes())?;
+
+    println!("All done! Created a new file: {}.ppm", target);
 
     Ok(())
 }
